78a59006b7581dad024c7b343d9a2c8703e63228
neveleneve <pandupandu813@gmail.com>
2020-10-02 14:05:14 +0700

    mini

--- resources/views/carapemesanan.blade.php
+++ resources/views/carapemesanan.blade.php
@@ -21,7 +21,18 @@
 </div>
 <section class="contact-section">
     <div class="container">
-
+        <div class="row">
+            <div class="col-12">
+                <img class="img-fluid" src="https://hijacksandals.com/wp-content/uploads/2020/08/hto-1x-1536x203.jpg" alt="">
+                <p>1. Bla bla bla</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8 offset-2">
+                
+                <iframe class="embed-responsive" src="https://drive.google.com/file/d/1FK6ganINiRCZRL_ohoRxs3qtesCTjyEw/preview" width="640" height="480"></iframe>
+            </div>
+        </div>
     </div>
 </section>
 @endsection
