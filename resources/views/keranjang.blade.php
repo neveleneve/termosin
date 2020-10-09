7557055eecf8edd77efcfb510f706742ac242419
neveleneve <pandupandu813@gmail.com>
2020-10-09 14:36:26 +0700

    mini

--- resources/views/keranjang.blade.php
+++ resources/views/keranjang.blade.php
@@ -63,58 +63,14 @@
                         <tr>
                             <td></td>
                             <td></td>
+                            <td></td>
                             <td>
                                 <h5>Subtotal</h5>
                             </td>
                             <td>
                                 <h5>$2160.00</h5>
                             </td>
-                        </tr>
-                        <tr class="shipping_area">
-                            <td></td>
-                            <td></td>
-                            <td>
-                                <h5>Shipping</h5>
-                            </td>
-                            <td>
-                                <div class="shipping_box">
-                                    <ul class="list">
-                                        <li>
-                                            Flat Rate: $5.00
-                                            <input type="radio" aria-label="Radio button for following text input">
-                                        </li>
-                                        <li>
-                                            Free Shipping
-                                            <input type="radio" aria-label="Radio button for following text input">
-                                        </li>
-                                        <li>
-                                            Flat Rate: $10.00
-                                            <input type="radio" aria-label="Radio button for following text input">
-                                        </li>
-                                        <li class="active">
-                                            Local Delivery: $2.00
-                                            <input type="radio" aria-label="Radio button for following text input">
-                                        </li>
-                                    </ul>
-                                    <h6>
-                                        Calculate Shipping
-                                        <i class="fa fa-caret-down" aria-hidden="true"></i>
-                                    </h6>
-                                    <select class="shipping_select">
-                                        <option value="1">Bangladesh</option>
-                                        <option value="2">India</option>
-                                        <option value="4">Pakistan</option>
-                                    </select>
-                                    <select class="shipping_select section_bg">
-                                        <option value="1">Select a State</option>
-                                        <option value="2">Select a State</option>
-                                        <option value="4">Select a State</option>
-                                    </select>
-                                    <input class="post_code" type="text" placeholder="Postcode/Zipcode" />
-                                    <a class="btn_1" href="#">Update Details</a>
-                                </div>
-                            </td>
-                        </tr>
+                        </tr>                        
                     </tbody>
                 </table>
                 <div class="checkout_btn_inner float-right">
