44bff3c75a46a4bade546d9f5ba26c834fe5b475
neveleneve <pandupandu813@gmail.com>
2020-10-10 21:57:43 +0700

    mini

--- resources/views/keranjang.blade.php
+++ resources/views/keranjang.blade.php
@@ -97,7 +97,7 @@
                 </table>
             </div>
             <div class="row">
-                <div class="col-5">
+                <div class="col-6">
 
                 </div>
                 <div class="col-3">
